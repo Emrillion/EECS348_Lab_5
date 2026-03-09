1ee7e8a9164ab2f40b196fbc6c5b80f337e5e254
Emrillion <simeongeib@ku.edu>
2026-03-08 22:13:39 -0500

    Fix function declarations

--- MatrixManipulation.c
+++ MatrixManipulation.c
@@ -1,38 +1,5 @@
 #include <stdio.h>
-
-int main() {
-    #define SIZE 5
-    int m1[SIZE][SIZE] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20},
-        {21, 22, 23, 24, 25}
-    };
-    int m2[SIZE][SIZE] = {
-        {25, 24, 23, 22, 21},
-        {20, 19, 18, 17, 16},
-        {15, 14, 13, 12, 11},
-        {10, 9, 8, 7, 6},
-        {5, 4, 3, 2, 1}
-    };
-
-    // Example usage:
-    printf("Addition\n");
-    int result[SIZE][SIZE];
-    addMatrices(m1, m2, result);
-    printMatrix(result);
-
-    printf("\nMultiplication\n");
-    multiplyMatrices(m1, m2, result);
-    printMatrix(result);
-
-    printf("\nTranspose of m1\n");
-    transposeMatrix(m1, result);
-    printMatrix(result);
-
-    return 0;
-}
+#define SIZE 5
 
 void addMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int result[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
@@ -68,4 +35,37 @@ void printMatrix(int m[SIZE][SIZE]) {
         }
         printf("\n");
     }
+}
+
+int main() {
+    int m1[SIZE][SIZE] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25}
+    };
+    int m2[SIZE][SIZE] = {
+        {25, 24, 23, 22, 21},
+        {20, 19, 18, 17, 16},
+        {15, 14, 13, 12, 11},
+        {10, 9, 8, 7, 6},
+        {5, 4, 3, 2, 1}
+    };
+
+    // Example usage:
+    printf("Addition\n");
+    int result[SIZE][SIZE];
+    addMatrices(m1, m2, result);
+    printMatrix(result);
+
+    printf("\nMultiplication\n");
+    multiplyMatrices(m1, m2, result);
+    printMatrix(result);
+
+    printf("\nTranspose of m1\n");
+    transposeMatrix(m1, result);
+    printMatrix(result);
+
+    return 0;
 }
